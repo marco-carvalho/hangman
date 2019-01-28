1dd68facb2c2dc70fed29840cdc0aadf2914945e
Marco Carvalho <marcolucio27@gmail.com>
2019-01-27 23:26:07 -0200

    create functions

--- src/hangman/core.clj
+++ src/hangman/core.clj
@@ -1,6 +1,17 @@
 (ns hangman.core
   (:gen-class))
 
+(defn endGame [] (print "You lose."))
+
+(defn game [lifes] 
+  (if (= vidas 0)
+    (endGame)
+    (do
+      (print lifes)
+      (game (dec lifes))
+    )
+  )
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
